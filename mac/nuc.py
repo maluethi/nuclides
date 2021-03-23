cc42ed45646a0439ee625e24adbe216e9e505994
maluethi <maluethi@protonmail.ch>
2021-03-23 23:27:07 +0100

    some more example usage

--- mac/nuc.py
+++ mac/nuc.py
@@ -16,12 +16,38 @@ from nuclides.nuclide import Nuclide
 import sqlalchemy as db
 
 from nuclides.element import Element
+from nuclides.nuclide import Nuclide
+from nuclides.util import _check_N_exists, _get_nuc_info
+
+nuc = Nuclide('Ti-48')
+print(nuc)
+
+nuc = Nuclide('Ti51')
+print(nuc)
+
 
 Ti = Element('Ti')
 print(Ti[46])
 print(Ti[59])
 
 Au = Element('Au')
-for nuc in Au:
-    print(nuc)
+for iso in Au:
+    print(iso)
+
+Zn = Element('Zn')
+
+decays = Ti[60].decays
+for dec in decays:
+    print(dec)
+
+T12 = Ti[44].decays[0].half_life
+BR = Ti[44].decays[0].branching_ratio
+
+print(f'T-44 most probable decay: half-life: {T12}s, branching ratio: {BR}')
+
+Si = Element('Si')
+print(Si[27])
+O = Element('O')
+print(O[28])
+
 
