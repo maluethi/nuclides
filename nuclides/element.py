2b06cd79d8d1b48175189d52428fdad150578128
maluethi <maluethi@protonmail.ch>
2021-03-24 21:58:53 +0100

    move fill_isotopes to utils

--- nuclides/element.py
+++ nuclides/element.py
@@ -1,18 +1,4 @@
-
-from .nuclide import Nuclide
-from . util import _get_Z, _get_name
-from dataclasses import dataclass
-from typing import List
-
-import sqlalchemy as db
-from sqlalchemy import and_
-from pkg_resources import resource_filename
-
-engine = db.create_engine( f'sqlite:///{resource_filename(__name__, "data/nuclides.db")}')
-connection = engine.connect()
-metadata = db.MetaData()
-elements = db.Table('elements', metadata, autoload=True, autoload_with=engine)
-nuclides_table = db.Table('nuclides', metadata, autoload=True, autoload_with=engine)
+from . util import _get_Z, _get_name, _get_isotopes
 
 
 class Element:
@@ -27,8 +13,8 @@ class Element:
         else:
             raise TypeError("Unknown type of nuclide specifier")
 
-        self.isotopes = sorted(self._get_isotopes(), key=lambda nu: nu.N)
-        self.isomers = sorted(self._get_isotopes(), key=lambda nu: nu.N)
+        self.isotopes = sorted(_get_isotopes(self.Z), key=lambda nu: nu.N)
+        self.isomers = sorted(_get_isotopes(self.Z, isomer=True), key=lambda nu: nu.N)      # TODO: This is failing!
         self._first_avail = self.isotopes[0].A
         self.n_nuclides = len(self.isotopes)
 
@@ -52,18 +38,3 @@ class Element:
             return res
         else:
             raise StopIteration
-
-    def _get_isotopes(self, isomer=False):
-        query = db.select([elements]).where(elements.columns.Z == self.Z)
-
-        res_prox = connection.execute(query)
-        elem_data = res_prox.fetchall()[0]
-
-        elem_nstart = elem_data[2]
-        elem_nrange = elem_data[3]
-
-        nuclides = []
-        for N in range(elem_nstart, elem_nstart + elem_nrange):
-            nuclides.append(Nuclide(N=N, Z=self.Z))
-
-        return nuclides
--- nuclides/util.py
+++ nuclides/util.py
@@ -1,4 +1,5 @@
 import sqlalchemy as db
+from . import nuclide
 from pkg_resources import resource_filename
 
 _engine = db.create_engine( f'sqlite:///{resource_filename(__name__, "data/nuclides.db")}')
@@ -39,6 +40,21 @@ def _get_nuc_info(Z, N, isomer=False):
             'mass_defect': res[5], 'mass_defect_error': res[6],
             'stable': res[7], 'abundance': res[8], 'abundance_error': res[9], 'isomer': res[10], '_nuc_id': res[0]}
 
+def _get_isotopes(Z, isomer=False):
+    query = db.select([_elements]).where(db.and_(_elements.columns.Z == Z,
+                                                 _nuclides_table.columns.isomer == isomer))
+
+    res_prox = _connection.execute(query)
+    elem_data = res_prox.fetchall()[0]
+
+    elem_nstart = elem_data[2]
+    elem_nrange = elem_data[3]
+
+    nuclides = []
+    for N in range(elem_nstart, elem_nstart + elem_nrange):
+        nuclides.append(nuclide.Nuclide(N=N, Z=Z))
+
+    return nuclides
 
 def _check_N_exists(N, **kwargs):
     if 'name' in kwargs:
